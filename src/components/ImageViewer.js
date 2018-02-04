78afa21e64ddae0702245805123c3c2b69f308e9
Christopher Henn <chris@chrishenn.net>
2018-02-03 16:36:47 -0800

    Add placeholder to ImageViewer

--- src/components/ImageViewer.js
+++ src/components/ImageViewer.js
@@ -4,7 +4,9 @@ import './ImageViewer.css'
 const ImageViewer = ({image}) => {
   return (
     <div className="image-viewer">
-      <img src={image.imgUrl} alt={image.description} />
+      <div className="image">
+        <img src={image.imgUrl} alt={image.description} />
+      </div>
       <div className="description">
         <span className="user">@{image.user}</span>
         {image.description}
